8e3063befb4924b6d8b6cdb2f801ce61ec293577
samarthhegdekalgar@gmail.com <samarthhegdekalgar@gmail.com>
2022-04-30 04:01:58 +0530

    feat: global fetch function

---
+++ src/network/fetchRequest.ts
@@ -0,0 +1,41 @@
+
+export interface requestInputType {
+    url: string
+    method?: 'GET' | 'POST' | 'PUT' | 'DELETE'
+    body?: string
+}
+
+/**
+ * This is the main function which makes all the api calls
+ * @param url - The url to be called
+ * @param method - The method to be used
+ * @param body - The body to be sent
+ * @returns - The response from the server
+ */
+export const fetchRequest = async<T>({ method = 'GET', url, body = undefined }: requestInputType): Promise<T> => {
+
+    /**
+     * we are using react query for this project and it is essential to return promise
+     * in any give time API might thorough error and it will be easier to handle it
+     * if we return promise from here
+     */
+    const responsePromise = await new Promise<T>((resolve, reject) => {
+        window.fetch(url, {
+            method,
+            body,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(async (response) => {
+            if (response.ok) {
+                resolve(await response.json() as T);
+                // This will execute if the request doesn't have any error but data might not be present in the response
+            } else {
+                reject(new Error(await response.text()));
+            }
+        }).catch((error) => {
+            reject(error);
+        })
+    })
+    return responsePromise;
+}
